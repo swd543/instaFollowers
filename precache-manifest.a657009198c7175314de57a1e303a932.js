cd1664c36635bc26a9da2b82c4500b1a3146bf92
Swapneel Datta <swd543@gmail.com>
2020-02-27 01:25:22 +0100

    Updates

---
+++ precache-manifest.a657009198c7175314de57a1e303a932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bf30cc2f23df0c6b73ce8f735ea485",
+    "url": "/instaFollowers/index.html"
+  },
+  {
+    "revision": "ca7649e74661b7b5fce4",
+    "url": "/instaFollowers/static/css/main.2d7d12eb.chunk.css"
+  },
+  {
+    "revision": "51381d8e3104c1bd1656",
+    "url": "/instaFollowers/static/js/2.64744148.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/instaFollowers/static/js/2.64744148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7649e74661b7b5fce4",
+    "url": "/instaFollowers/static/js/main.7f10ec23.chunk.js"
+  },
+  {
+    "revision": "060fbf0f17c0798a8275",
+    "url": "/instaFollowers/static/js/runtime-main.76308c0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/instaFollowers/static/media/logo.5d5d9eef.svg"
+  }
+]);
